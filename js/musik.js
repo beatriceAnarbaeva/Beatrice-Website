36af239356dc296b13583a629310e57c429170fc
beatriceAnarbaeva <beaanarb@leo-statz-berufskolleg.de>
2025-12-17 22:21:54 +0100

    Add files via upload

---
+++ js/musik.js
@@ -0,0 +1,20 @@
+
+const music = document.getElementById('bg-music');
+const btn = document.getElementById('music-btn');
+let playing = false;
+
+btn.addEventListener('click', () => {
+  if (!playing) {
+    music.play();
+    btn.textContent = "⏸️ Musik pausieren";
+    playing = true;
+  } else {
+    music.pause();
+    btn.textContent = "🎵 Musik starten";
+    playing = false;
+  }
+});
+
+
+
+
